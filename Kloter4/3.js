6115ed4e921a957890f9457cd5276b0ad1569612
Syaifudin Fendi Prasetyo <fendypras351@gmail.com>
2020-01-18 22:11:22 +0700

    Kloter 4

--- Kloter4/3.js
+++ Kloter4/3.js
@@ -1,5 +1,10 @@
+<textarea id="cerita"></textarea><br>
+<input type="button" value="Hitung Banyak Kata" onClick="hitung()"> <input type="text" id="total" readonly>
+
+<script type="text/javascript">
 function hitung(){
     var banyak_kata = document.getElementById('cerita').value
     banyak_kata = banyak_kata.split(" ")
     document.getElementById('total').value = banyak_kata.length
 }
+</script>
